5fe1b0d6c7b800e246d891c7f8412620c3ddd341
loebd <dorian.loeb@etu.univ-amu.fr>
2024-12-02 09:45:03 +0100

    javadoc Proba.java

--- src/controller/Proba.java
+++ src/controller/Proba.java
@@ -1,34 +1,25 @@
 package controller;
 
 public class Proba {
+    /**
+     * Calcul de la probabilité de réussite d'une action en fonction de la qualité du service  et du ratio maladie.
+     *
+     * @param qualite La qualité de l'objet.
+     * @param r       La rareté de l'objet.
+     * @return La probabilité de réussite de l'action.
+     */
     public double calculProba(String qualite, double r){
-        double p = 0;
-        switch (qualite){
-            case "Inexistant":
-                p = 0.35;
-                break;
-            case "Mediocre":
-                p = 0.45;
-                break;
-            case "Insuffisant":
-                p = 0.55;
-                break;
-            case "Faible":
-                p = 0.65;
-                break;
-            case "Moyen":
-                p = 0.75;
-                break;
-            case "Bon":
-                p = 0.8;
-                break;
-            case "Super":
-                p = 0.9;
-                break;
-            case "Parfait":
-                p = 1.0;
-                break;
-        }
+        double p = switch (qualite) {
+            case "Inexistant" -> 0.35;
+            case "Mediocre" -> 0.45;
+            case "Insuffisant" -> 0.55;
+            case "Faible" -> 0.65;
+            case "Moyen" -> 0.75;
+            case "Bon" -> 0.8;
+            case "Super" -> 0.9;
+            case "Parfait" -> 1.0;
+            default -> 0;
+        };
 
         return p*(1-(r-0.1));
     }
